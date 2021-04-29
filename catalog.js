6a94ddf36eeac25a8760b84940e8b6aed072ad52
tejveer1031 <tejihayer.92@gmail.com>
2021-04-29 02:37:07 -0400

    y8f

--- catalog.js
+++ catalog.js
@@ -2,39 +2,39 @@ function loadProducts() {
     let products = [
         {
             'id': 0,
-            'name': 'Red Jersey',
-            'description': 'Manchester United Home Jersey, red, sponsored by Chevrolet',
+            'name': '',
+            'description': '',
             'price': 59.99,
             'pic': 'download.jfif',
             'Stock': 200
         },
         {
             'id': 1,
-            'name': 'White Jersey',
-            'description': 'Manchester United Away Jersey, white, sponsored by Chevrolet',
+            'name': '',
+            'description': '',
             'price': 49.99,
             'pic': 'download2.jfif',
             'Stock': 133
         },
         {
             'id': 2,
-            'name': 'Black Jersey',
-            'description': 'Manchester United Extra Jersey, black, sponsored by Chevrolet',
+            'name': '',
+            'description': '',
             'price': 54.99,
             'pic': 'download3.jfif',
             'Stock': 544
         },
         {
             'id': 3,
-            'name': 'Blue Jacket',
-            'description': 'Blue Jacket for cold and raniy weather',
+            'name': 't',
+            'description': '',
             'price': 129.99,
             'pic': 'images.jfif',
             'Stock': 14
         },
         {
             'id': 4,
-            'name': 'Snapback Cap',
+            'name': '',
             'description': '',
             'price': 24.99,
             'pic': 'images2.jfif',
@@ -42,7 +42,7 @@ function loadProducts() {
         },
         {
             'id': 5,
-            'name': 'Champion Flag',
+            'name': '',
             'description': '',
             'price': 24.99,
             'pic': 'images3.jfif',
@@ -50,7 +50,7 @@ function loadProducts() {
         },
         {
             'id': 6,
-            'name': 'Champion Flag',
+            'name': '',
             'description': '',
             'price': 50,
             'pic': 'images7.jfif',
